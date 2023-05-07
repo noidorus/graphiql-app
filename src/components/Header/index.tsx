053416171e05aa91588f5472b3d1ab8dda4e4b01
Maria Gornashevich <lightbrown9874@gmail.com>
2023-05-07 03:38:56 +0300

    feat: add logic for header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import styles from './styles.module.scss';
+import { auth } from '@/services/firebase';
+import { User } from 'firebase/auth';
 
 const Header = () => {
   const [isSticky, setIsSticky] = useState(false);
+  const router = useRouter();
+  const [user, setUser] = useState<User | null>(null);
 
   const handleScroll = () => {
     if (window.scrollY > 0) {
@@ -19,16 +27,45 @@ const Header = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => setUser(user));
+
+    return unsubscribe;
+  }, []);
+
+  const handleSignOut = async () => {
+    try {
+      await auth.signOut();
+      setUser(null);
+      router.push('/welcome');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className={`${styles.header} ${isSticky ? `${styles['header_sticky']}` : ''}`}>
       <img className={styles['header__logo']} src="/logo.svg" alt="logo" />
       <div className={styles['header__btns']}>
-        <button className={styles['header__btn']} onClick={() => {}}>
-          Sign In
-        </button>
-        <button className={styles['header__btn']} onClick={() => {}}>
-          Sign Up
-        </button>
+        {!user ? (
+          <>
+            <button className={styles['header__btn']} onClick={() => router.push('/login')}>
+              Sign In
+            </button>
+            <button className={styles['header__btn']} onClick={() => router.push('/registration')}>
+              Sign Up
+            </button>
+          </>
+        ) : (
+          <>
+            <button className={styles['header__btn']} onClick={handleSignOut}>
+              Sign Out
+            </button>
+            <button className={styles['header__btn']} onClick={() => router.push('/main')}>
+              Go to Main Page
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
