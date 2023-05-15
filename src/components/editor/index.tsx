74763bba42093b0994687a0ed9c2c43bbcec042b
Maria Gornashevich <lightbrown9874@gmail.com>
2023-05-15 03:27:12 +0300

    feat:add layout for main page

---
+++ src/components/editor/index.tsx
@@ -0,0 +1,84 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+import styles from './style.module.scss';
+
+const MIN_BLOCK_WIDTH = 280;
+
+const Editor = () => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [editorsWidth, setEditorsWidth] = useState<string>('50%');
+  const [responseWidth, setResponseWidth] = useState<string>('50%');
+  const dragBarRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+    setIsDragging(true);
+  };
+
+  const handleMouseUp = (event: MouseEvent) => {
+    setIsDragging(false);
+  };
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      if (isDragging) {
+        const dragBarWidth = dragBarRef.current?.offsetWidth ?? 0;
+        const containerWidth = containerRef.current?.offsetWidth ?? 0;
+        const mouseX = event.pageX;
+        const containerX = containerRef.current?.getBoundingClientRect().x ?? 0;
+        const dragBarX = mouseX - containerX;
+
+        let newEditorsWidth = `${(dragBarX / containerWidth) * 100}%`;
+        let newResponseWidth = `${100 - (dragBarX / containerWidth) * 100}%`;
+
+        const editorsWidthPx = (parseInt(newEditorsWidth) / 100) * containerWidth;
+        const responseWidthPx = (parseInt(newResponseWidth) / 100) * containerWidth;
+        if (editorsWidthPx < MIN_BLOCK_WIDTH) {
+          newEditorsWidth = `${(MIN_BLOCK_WIDTH / containerWidth) * 100}%`;
+          newResponseWidth = `${100 - (MIN_BLOCK_WIDTH / containerWidth) * 100}%`;
+        } else if (responseWidthPx < MIN_BLOCK_WIDTH) {
+          newEditorsWidth = `${100 - (MIN_BLOCK_WIDTH / containerWidth) * 100}%`;
+          newResponseWidth = `${(MIN_BLOCK_WIDTH / containerWidth) * 100}%`;
+        }
+
+        setEditorsWidth(newEditorsWidth);
+        setResponseWidth(newResponseWidth);
+      }
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [isDragging]);
+
+  return (
+    <div className={styles.block} ref={containerRef}>
+      <div className={styles.editors} style={{ minWidth: MIN_BLOCK_WIDTH, width: editorsWidth }}>
+        <div className={styles['editors__editor-block']}>
+          <div className={styles['editors__editor']}>editor </div>
+          <div className={styles['editors__editor-toolbar']}>
+            <button className={styles['editors__editor-button_start']}>
+              <img src="/play.png" alt="start" />
+            </button>
+          </div>
+        </div>
+
+        <div className={styles['editors__editor-tools']}>
+          <button className={styles['editors__editor-tools__item']}>Variables</button>
+          <button className={styles['editors__editor-tools__item']}>Headers</button>
+        </div>
+        <div className={styles['editors__editor-tool']}>1.</div>
+      </div>
+      <div className={styles.dragbar} ref={dragBarRef} onMouseDown={handleMouseDown}></div>
+      <div className={styles.response} style={{ width: responseWidth }}>
+        response
+      </div>
+    </div>
+  );
+};
+
+export default Editor;
--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -5,6 +5,10 @@ import Header from '@/components/Header';
 import PageContainer from '@/components/PageContainer';
 import { firebaseAdmin } from '@/firebase/firebaseAdmin';
 
+import styles from './style.module.scss';
+import Footer from '@/components/Footer';
+import Editor from '@/components/editor';
+
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   try {
     const cookies = nookies.get(ctx);
@@ -29,7 +33,20 @@ const AppPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) 
   return (
     <>
       <Header />
-      <PageContainer>Hello</PageContainer>
+      <PageContainer>
+        <div className={styles.app}>
+          <div className={styles['app__sidebar']}>
+            <button className={styles.app__sidebar_docs}>
+              <img className={styles['app__sidebar__img']} src="/docs.png" alt="docs" />
+            </button>
+            <button className={styles.app__sidebar_refetch}>
+              <img className={styles['app__sidebar__img']} src="/refetch.svg" alt="docs" />
+            </button>
+          </div>
+          <Editor />
+        </div>
+      </PageContainer>
+      <Footer />
     </>
   );
 };
