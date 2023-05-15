c5889e7ee85abacae1827c49b9ed0d743f2489fd
Maria Gornashevich <lightbrown9874@gmail.com>
2023-05-15 12:58:29 +0300

    feat: add translation to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import { useTranslation } from 'react-i18next';
 import Button from '@/components/Button';
 import PageContainer from '@/components/PageContainer';
 
@@ -6,14 +7,15 @@ import styles from './404.module.scss';
 
 const ErrorPage = () => {
   const router = useRouter();
+  const { t } = useTranslation();
 
   return (
     <PageContainer>
       <div className={styles.error}>
         <img className={styles['error__img']} src="/error.png" alt="error" />
         <div className={styles['error__block']}>
-          <h1 className={styles['error__title']}>Oops!</h1>
-          <p className={styles['error__text']}>You are lost</p>
+          <h1 className={styles['error__title']}>{t('404-oops')}</h1>
+          <p className={styles['error__text']}>{t('404-error')}</p>
           <Button type="button" onClick={() => router.push('/')} text={'Go Home'} />
         </div>
       </div>
