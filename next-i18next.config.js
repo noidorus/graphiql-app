87f06c75ed226e4039088d24c314f1da860fb585
Maria Gornashevich <lightbrown9874@gmail.com>
2023-05-29 02:47:35 +0300

    fix: config for i18

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     locales: ['default', 'en', 'ru'],
     defaultLocale: 'en',
     localeDetection: false,
+    localePath: path.resolve('./public/locales'),
   },
 };
